6e4facf349634e02b21d65fa78ccd15e836db23d
ducnguyen8817 <duc.nguyen@lgbs.com>
2018-06-04 18:10:26 -0500

    06/04/2018

--- src/FunctionalInterface.java
+++ src/FunctionalInterface.java
@@ -1,6 +1,16 @@
 /**
  * Created by Duc.Nguyen on 6/4/2018.
  */
-public class FunctionalInterface {
-    int temp;
+public interface FunctionalInterface {
+    // SAM Single Abstract Method interfaces
+    // These can be represented using Lambda expressions
+    // Only one abstract method is allowed in any functional interface
+    // Second abstract method is not permitted in a functional interface
+    // Default methods have an implementation, they are not abstract
+
+    public void firstWork();
+    default void doSomeWork(){
+        System.out.println("Do some homework");
+    }
+
 }
---
+++ src/Optionals.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Duc.Nguyen on 6/4/2018.
+ */
+public class Optionals {
+    // Optional is a way of replacing a nullable T reference with a non null value
+    // An optional can contain a non - null T reference
+    // or it may contain nothing
+    //
+}

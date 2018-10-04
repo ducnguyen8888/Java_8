8e6098868481a330146ddf1f5cfef5feea45ca2f
ducnguyen8817 <duc.nguyen@lgbs.com>
2018-10-04 17:15:19 -0500

    06/04/2018

--- src/Concurrency.java
+++ src/Concurrency.java
@@ -4,7 +4,7 @@ import java.util.concurrent.*;
  * Created by Duc.Nguyen on 10/4/2018.
  */
 public class Concurrency  {
-    public static void ConcurrencyPool()throws InterruptedException, ExecutionException{
+    public static void concurrencyPool()throws InterruptedException, ExecutionException{
         ExecutorService service = Executors.newFixedThreadPool(15);
         long start = System.currentTimeMillis();
         System.out.println("Start at: "+ start);
@@ -35,8 +35,41 @@ public class Concurrency  {
         System.out.println("End at: " + (System.currentTimeMillis()-start));
     }
 
-    public static void main(String [] args) throws InterruptedException, ExecutionException {
+    public static void result() throws InterruptedException, ExecutionException{
+        ExecutorService service = null;
+        try {
+            service= Executors.newSingleThreadExecutor();
+            Future<?> result = service.submit(()->{
+                for (int i = 0; i<1000; i++){
+                    System.out.println("Still processing");
+                }
+            });
+            result.get(500, TimeUnit.MILLISECONDS);
+            System.out.println("Complete processing");
+        } catch (TimeoutException e){
+            System.out.println("Not completed in time");
+        } finally {
+            if (service != null){
+                service.shutdown();
+            }
+        }
+    }
 
+    public static void singleThread() throws InterruptedException{
+        ExecutorService service = null;
+        try {
+            service = Executors.newSingleThreadExecutor();
+            System.out.println("Start");
+            service.execute(()->{for(int i = 0; i <30; i++){System.out.println("Processing number "+i);}});
+            System.out.println("Are numbers processed completely");
+
+        } finally {
+            if ( service != null){ service.shutdown();}
+        }
+    }
+
+    public static void main(String [] args) throws InterruptedException, ExecutionException {
+        result();
     }
 
     private static void sleep(){
